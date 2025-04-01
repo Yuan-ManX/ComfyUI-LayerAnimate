d8c4b225db41c7232b7f0fa8f351ec536a087dd6
Yuan-Man <68322456+Yuan-ManX@users.noreply.github.com>
2025-04-01 11:10:24 +0800

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,1 +1,15 @@
+from .LayerAnimate_nodes import LoadImages, LoadPretrainedModel, LayerAnimateNode
 
+NODE_CLASS_MAPPINGS = {
+    "LoadImages": LoadImages,
+    "LoadPretrainedModel": LoadPretrainedModel,
+    "LayerAnimateNode": LayerAnimateNode,
+}
+
+NODE_DISPLAY_NAME_MAPPINGS = {
+    "LoadImages": "Load Images",
+    "LoadPretrainedModel": "Load Pretrained Model",
+    "LayerAnimateNode": "Layer Animate Process",
+} 
+
+__all__ = ['NODE_CLASS_MAPPINGS', 'NODE_DISPLAY_NAME_MAPPINGS']
